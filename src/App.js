b608e6d530a497de6ee038d1699823d73654e399
Askarii <muhammadaskari267@gmail.com>
2021-04-30 09:36:21 +0500

    All about redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, makeStyles, Paper, Typography } from '@material-ui/core'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement } from './redux/counter'
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: theme.spacing(2),
+    color: 'black',
+    background: 'gray',
+    padding: theme.spacing(2)
+  },
+  box: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: theme.spacing(2),
+  },
+
+
+}))
+
+const App = () => {
+  const classes = useStyles()
+  const { count } = useSelector(state => state.counter)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Paper className={classes.root}>
+        <Box>
+          <Typography variant="h5">Count:{count}</Typography>
+        </Box>
+        <Box className={classes.box}>
+          <Button type="submit" variant="contained" onClick={() => dispatch(increment())}>Increment</Button>
+          <Button type="submit" variant="contained" onClick={() => dispatch(decrement())}>Decrement</Button>
+        </Box>
+      </Paper>
+    </Container>
+  )
 }
 
-export default App;
+export default App
